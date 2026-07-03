41ed6a0b45a8499afe5526d7fcb0a4c474a5da67
Benoit Lafrance <43009237+blafr103@users.noreply.github.com>
2026-07-03 17:20:12 -0400

    formatting

--- labs/lab2/FourierF1.java
+++ labs/lab2/FourierF1.java
@@ -1,16 +1,15 @@
 public class FourierF1 {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    double a0 = 0.0;
-    double an = 0.0;
-    double sum = 0.0;
-    
-    for (int n = 1; n <= 100; n++) {
-      double bn = (-20 * Math.cos(n * Math.PI)) / (n * Math.PI);
-      sum += a0 + an*Math.cos(n*Math.PI)+bn*Math.sin(n*Math.PI);
-    }
-    
+		double a0 = 0.0;
+	    double an = 0.0;
+	    double sum = 0.0;
+	    
+	    for (int n = 1; n <= 100; n++) {
+	      double bn = (-20 * Math.cos(n * Math.PI)) / (n * Math.PI);
+	      sum += a0 + an*Math.cos(n*Math.PI)+bn*Math.sin(n*Math.PI);
+	    }
 		System.out.println("The Fourier series up to the 100th harmonic is: "+sum);
-  }
+  	}
 }
