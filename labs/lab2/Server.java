e60e49d3392a88e26528e2781b1df1f0300342e5
Benoit Lafrance <43009237+blafr103@users.noreply.github.com>
2026-07-03 17:49:12 -0400

    Refactor Server to receive and print waveforms
    Refactored Server class to handle a fixed number of waveforms and improved resource management with try-with-resources.

--- labs/lab2/Server.java
+++ labs/lab2/Server.java
@@ -1,19 +1,21 @@
 import java.net.*;
 import java.io.*;
+
 public class Server {
-    public static void main(String[] args){
-        try{
-            int port=4444;
-            ServerSocket socket=new ServerSocket(port);
-            Socket sSocket=socket.accept();
-            DataInputStream data=new DataInputStream(sSocket.getInputStream());
-            DataOutputStream dataout=new DataOutputStream(sSocket.getOutputStream());
-            while(true){
-                    String tmp= (String)data.readUTF();
-                     dataout.writeUTF(tmp);
+    public static void main(String[] args) {
+        int port = 4444;
+        int functionCount = 6;   // fixed set of six waveforms
+
+        try (ServerSocket socket = new ServerSocket(port);
+             Socket sSocket = socket.accept();
+             DataInputStream data = new DataInputStream(sSocket.getInputStream())) {
+
+            // this machine is the remote display: receive each curve and print it
+            for (int i = 0; i < functionCount; i++) {
+                System.out.println(data.readUTF());
             }
-        }
-        catch(Exception e){
+
+        } catch (IOException e) {
             System.out.println(e);
         }
     }
